1a130af6d7d1362e069551c4dbd304cf2d1fed54
Josh King <jking@chambana.net>
2019-10-20 16:53:09 -0400

    Make async and fix race condition when file does not exist yet.

--- brain.js
+++ brain.js
@@ -18,16 +18,23 @@ const path = require('path');
 
 module.exports = function(robot) {
   let brainPath = process.env.HUBOT_FILEBRAIN_PATH || process.cwd();
-  brainPath = path.join(brainPath, 'brain.json');
-  try {
-    const data = fs.readFileSync(brainPath, 'utf-8');
-    if (data) {
+  brainPath = path.resolve(brainPath, 'brain.json');
+  robot.logger.debug("Loading file-backed brain from ", brainPath);
+  fs.promises.access(brainPath, fs.constants.R_OK | fs.constants.W_OK).then(() => {
+    fs.promises.readFile(brainPath, 'utf-8').then(data => {
+      robot.logger.debug("Merging saved data.");
       robot.brain.mergeData(JSON.parse(data));
+    });
+  }).catch(error => {
+    if (error.code !== 'ENOENT') {
+      robot.logger.error('Unable to read file', error);
+    } else {
+      robot.brain.mergeData({});
     }
-  } catch (error) {
-    if (error.code !== 'ENOENT') { console.log('Unable to read file', error); }
-  }
+  });
   // add the option Readable, will output an indented json
-  return robot.brain.on('save', data => fs.writeFileSync(brainPath, JSON.stringify(data, null, 4), 'utf-8'));
+  return robot.brain.on('save', data => {
+    fs.writeFileSync(brainPath, JSON.stringify(data, null, 4), 'utf-8');
+  });
 };
 
